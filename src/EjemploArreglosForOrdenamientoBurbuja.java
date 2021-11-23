50a2298342819c8a429e0fb1f3a493b5fb5ddba7
Leon Matias <leon.matias.dev@gmail.com>
2021-11-23 09:20:31 -0500

    ArreglosForOrdenamientoBurbuja

--- src/EjemploArreglosForOrdenamientoBurbuja.java
+++ src/EjemploArreglosForOrdenamientoBurbuja.java
@@ -27,12 +27,12 @@ public class EjemploArreglosForOrdenamientoBurbuja {
 
         int contador = 0;
 
-        for (int i = 0; i < total; i++) {
-            for (int j = 0; j < total; j++) {
-                if (productos[i].compareTo(productos[j]) < 0) {
+        for (int i = 0; i < total - 1; i++) {
+            for (int j = 0; j < total - 1 - i; j++) {
+                if (productos[i + 1].compareTo(productos[j]) < 0) {
                     String auxiliar = productos[i];
                     productos[i] = productos[j];
-                    productos[j] = auxiliar;
+                    productos[j + 1] = auxiliar;
                 }
                 contador++;
             }
